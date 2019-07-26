5a0539bde075ade34ffa721e3b4cadc8fd36a6f0
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-07-27 03:18:43 +0530

    practice

---
+++ app/src/main/java/com/example/viewbinding/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.viewbinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = "BIN";
+    Button btn2;
+    Button btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v ){
+                Log.d(TAG, "Second Button Clicked");
+            }
+        });
+        btn3.setOnClickListener(this);
+    }
+
+    public void onButtonClicked(View v){
+        Log.d(TAG, "Button Clicked ");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "3rd Button clicked");
+    }
+}
